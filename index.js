a57e8fcc65ab0cc98be8c398be7bc7750c663cd3
Zech Drinkhall <zechdrink@aol.com>
2019-03-18 12:16:07 -0700

    created a get endpoint

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// implement your API here
+// implement your API here\
+const express = require('express');
+
+const server = express();
+
+const db = require('./data/db.js');
+
+server.use(express.json());
+
+
+server.get('/api/users', (req, res) => {
+    db.users.find().then(users => {
+        res.status(200).json(users);
+    }).catch(error => {
+        res.status(500).json({
+            message: 'error retrieving hubs', error
+        })
+    })
+})
+
+server.post('/api/users', (req, res) =>{
+    const userInfo = req.body;
+    console.log('user info', userInfo);
+
+    db.users
+    .add(userInfo)
+    .then(users => {
+        res.status(201).json(users)
+    })
+});
+
+
+server.listen(5000, () => {
+    console.log('\n** API up and running')
+});
