a463b2f740eb6897c9cf3b2a9823a94d713a8ec0
Zech Drinkhall <zechdrink@aol.com>
2019-03-18 15:51:06 -0700

    finished the update and get by Id endpoints

--- index.js
+++ index.js
@@ -13,21 +13,81 @@ server.get('/api/users', (req, res) => {
         res.status(200).json(users);
     }).catch(error => {
         res.status(500).json({
-            message: 'error retrieving hubs', error
+            message: 'error retrieving user', error
         })
     })
 })
 
-server.post('/api/users', (req, res) =>{
-    const userInfo = req.body;
-    console.log('user info', userInfo);
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
 
-    db.users
-    .add(userInfo)
-    .then(users => {
-        res.status(201).json(users)
+   db.users.findById(id).then(user => {
+        if(!user) {
+            return  res.status(404).json({ message: 'This user is dead'})
+        }
+        else {
+       res.status(200).json(user);
+        }
+  
+  }).catch(error => {
+    res.status(500).json({
+        message: 'error retrieving hubs', error
     })
+})
+
+})
+
+
+
+
+server.post('/api/users', (req, res) => {
+    const userInfo = req.body;
+
+    if(!req.body.name || !req.body.bio){
+        res.status(400).json({ message: 'name and bio required' })
+    } else {
+      
+    console.log('user info', userInfo);
+    db.users.insert(userInfo)
+            .then(user => {
+                res.status(201).json(user);
+        })
+                .catch(error => {
+                    res.status(500).json({message: 'error retrieving user', error})
+                }) 
+            }
+
 });
+
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.users.remove(id).then(deleted => {
+        res.status(204).end()
+    }).catch(error => {
+        res.status(500).json({message: "The user could not be removed"})
+    })
+    
+})
+
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const change = req.body;
+    console.log(id);
+    db.users
+    .update(id, change)
+    .then(updated => {
+    if (updated) {
+        res.status(200).json(updated);
+        } else {
+        res.status(404).json({ message: 'user not found' });
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'error updating user' });
+    });
+})
+
 
 
 server.listen(5000, () => {
